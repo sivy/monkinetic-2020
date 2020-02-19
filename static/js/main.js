c8454b178d88d8c068a41371c7ffe9a396484118
Steve Ivy <steveivy@gmail.com>
2020-02-18 20:32:46 -0700

    redo js-based note/post form

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,13 @@ $(".chars").text($("#notecontent").val().length)
 $("#notecontent").on("keyup", function () {
     $(".chars").text($(this).val().length)
 })
+
+$("#post-tabs button").on("click", function () {
+    $(".contentform").css("display", "none")
+    var target = $(this).attr("data-target")
+    var dim = $(this).attr("data-dim")
+    console.log(target)
+    $(this).css("opacity", "1.0")
+    $(target).css("display", "block")
+    $(dim).css("opacity", "0.5")
+})
