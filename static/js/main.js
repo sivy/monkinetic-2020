e756d8073d107daf10585ae6760770b4267b5759
Steve Ivy <steveivy@gmail.com>
2020-02-20 13:27:38 -0700

    experimental post UI

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,24 @@
-$(".chars").text($("#notecontent").val().length)
+$(".chars").text($("#postcontent").val().length)
+$("#title-form, #slug-form, #tag-form").css("display", "none")
 
-$("#notecontent").on("keyup", function () {
-    $(".chars").text($(this).val().length)
+$("#postcontent").on("keyup", function () {
+    var count = $(this).val().length
+    $(".chars").text(count)
+    if (count > 280) {
+        $("#title-form, #slug-form").css("display", "block")
+        $("#postcontent").attr("rows", 20)
+    } else {
+        $("#title-form, #slug-form").css("display", "none")
+        $("#postcontent").attr("rows", 10)
+    }
 })
 
-$("#post-tabs button").on("click", function () {
-    $(".contentform").css("display", "none")
-    var target = $(this).attr("data-target")
-    var dim = $(this).attr("data-dim")
-    console.log(target)
-    $(this).css("opacity", "1.0")
-    $(target).css("display", "block")
-    $(dim).css("opacity", "0.5")
-})
+
+// $("#post-tabs button").on("click", function () {
+//     var target = $(this).attr("data-target")
+//     var dim = $(this).attr("data-dim")
+//     console.log(target)
+//     $(this).css("opacity", "1.0")
+//     $(target).css("display", "block")
+//     $(dim).css("opacity", "0.5")
+// })
