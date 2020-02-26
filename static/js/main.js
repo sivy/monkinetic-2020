dd4a8539366e8b5adbfdc715d8fee058828ab20f
Steve Ivy <steveivy@gmail.com>
2020-02-26 07:14:22 -0700

    make the posting UI a bit nicer, check an uncheck the default syndication options

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,27 @@
 $(".chars").text($("#postcontent").val().length)
 $("#title-form, #slug-form, #tag-form").css("display", "none")
+var inputs = $("input[name=twitter], input[name=mastodon]").dom
+for (const i of inputs) {
+    i.checked = true
+}
+
 
 $("#postcontent").on("keyup", function () {
     var count = $(this).val().length
     $(".chars").text(count)
     if (count > 280) {
+        // enable Post options
         $("#title-form, #slug-form").css("display", "block")
         $("#postcontent").attr("rows", 20)
+        for (const i of inputs) {
+            i.checked = false
+        }
     } else {
+        // enabled Note options
         $("#title-form, #slug-form").css("display", "none")
         $("#postcontent").attr("rows", 10)
+        for (const i of inputs) {
+            i.checked = true
+        }
     }
 })
-
-
-// $("#post-tabs button").on("click", function () {
-//     var target = $(this).attr("data-target")
-//     var dim = $(this).attr("data-dim")
-//     console.log(target)
-//     $(this).css("opacity", "1.0")
-//     $(target).css("display", "block")
-//     $(dim).css("opacity", "0.5")
-// })
